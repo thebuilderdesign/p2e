a689df985c15d61fb393709c5bcd3473ef2546ea
thebuilderdesign <92240812+thebuilderdesign@users.noreply.github.com>
2023-03-25 10:29:17 +0700

    Add files via upload

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { CoreBlockchain } from "@thirdweb-dev/chains";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThirdwebProvider activeChain={ CoreBlockchain }>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;
